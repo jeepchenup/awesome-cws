d58eb6dccf258eed0f044fea77d12cfb99d39ed3
Steven Chen <jeepchenup@foxmail.com>
2019-01-23 20:28:10 +0800

    rename package in the awsome-cws-common module.

--- awsome-cws-common/src/main/java/org/awsome/cws/common/utils/DateUtils.java
+++ awsome-cws-common/src/main/java/org/awsome/cws/common/utils/DateUtils.java
@@ -1,15 +1,17 @@
-package org.awsome.cws.common;
+package org.awsome.cws.common.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.awsome.cws.common.constants.DateConstants;
 
 public class DateUtils {
 
 	public static final ThreadLocal<SimpleDateFormat> formart = new ThreadLocal<SimpleDateFormat>() {
 		@Override
 		protected SimpleDateFormat initialValue() {
-			String pattern = "yyyy-MM-dd";
+			String pattern = DateConstants.YYYY_MM_DD_PATTERN;
 			return new SimpleDateFormat(pattern);
 		}
 	};
