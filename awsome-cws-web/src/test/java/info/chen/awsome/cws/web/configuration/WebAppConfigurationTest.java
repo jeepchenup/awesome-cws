852dba568480428e592d0794c89ac7cf0855898f
Steven Chen <jeepchenup@foxmail.com>
2019-01-29 23:35:34 +0800

    security demo bug

--- awsome-cws-web/src/test/java/info/chen/awsome/cws/web/configuration/WebAppConfigurationTest.java
+++ awsome-cws-web/src/test/java/info/chen/awsome/cws/web/configuration/WebAppConfigurationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import info.chen.awsome.cws.security.configuration.CWSSecurityConfiguration;
 import info.chen.awsome.cws.service.EmployeeService;
 import info.chen.awsome.cws.web.configuration.WebAppConfiguration;
 import info.chen.awsome.cws.web.controller.EmployeeController;
@@ -26,9 +27,14 @@ public class WebAppConfigurationTest {
 	@Autowired
 	private EmployeeController employeeController;
 	
+	@Autowired
+	private CWSSecurityConfiguration securityConfiguration;
+	
 	@Test
 	public void testWebAppConfigurationNotNull() {
 		assertNotNull(webAppConfiguration);
+		assertNotNull(employeeService);
 		assertNotNull(employeeController);
+		assertNotNull(securityConfiguration);
 	}
 }
