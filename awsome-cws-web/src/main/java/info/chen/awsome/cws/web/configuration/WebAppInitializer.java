854d4e90270d38c7a51ff98563184fc9364743fa
Steven Chen <jeepchenup@foxmail.com>
2019-01-30 21:00:47 +0800

    fix bug No bean named 'springSecurityFilterChain' is defined

--- awsome-cws-web/src/main/java/info/chen/awsome/cws/web/configuration/WebAppInitializer.java
+++ awsome-cws-web/src/main/java/info/chen/awsome/cws/web/configuration/WebAppInitializer.java
@@ -1,6 +1,8 @@
 package info.chen.awsome.cws.web.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import info.chen.awsome.cws.security.configuration.CWSSecurityConfiguration;
 /**
  * Assemble the web application's moving parts, like what web.xml does.
  */
@@ -9,7 +11,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// load other beans just like datasource, sessionFactory
-		return new Class[] { HibernateConfiguration.class, AOPConfiguration.class };
+		return new Class[] { HibernateConfiguration.class, AOPConfiguration.class, CWSSecurityConfiguration.class };
 	}
 
 	@Override
