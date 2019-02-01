46731c6c8b0764c6a4b1e221b1746138bf0f9ff1
Steven Chen <jeepchenup@foxmail.com>
2019-02-01 12:37:08 +0800

    commit

--- awsome-cws-security/src/main/java/info/chen/awsome/cws/security/configuration/MultiHttpSecurityConfigration.java
+++ awsome-cws-security/src/main/java/info/chen/awsome/cws/security/configuration/MultiHttpSecurityConfigration.java
@@ -1,5 +1,6 @@
 package info.chen.awsome.cws.security.configuration;
 
+import org.awsome.cws.common.constants.APISecurityConstatnts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,14 +9,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
+/**
+ * 1. RESTFUL API close the CSRF function and be authenticated by basic HTTP.
+ * 2. Others API open then CSRF function and be authenticated by LoginForm.
+ */
 @EnableWebSecurity
 @Configuration
 public class MultiHttpSecurityConfigration {
 	
-//	@Autowired
-//	private static RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-
 	@Autowired
 	public void registerGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
@@ -31,16 +32,13 @@ public class MultiHttpSecurityConfigration {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable()
-//				.exceptionHandling()
-//					.authenticationEntryPoint(restAuthenticationEntryPoint)
-//				.and()
-				.antMatcher("/rest/**")
+				.antMatcher(APISecurityConstatnts.RESTFUL_API)
 				.authorizeRequests()
 					.anyRequest().authenticated()
 				.and()
 				.httpBasic();
 		}
-
+		
 	}
 
 	@Configuration
--- awsome-cws-security/src/main/java/info/chen/awsome/cws/security/entrypoint/RestAuthenticationEntryPoint.java
+++ awsome-cws-security/src/main/java/info/chen/awsome/cws/security/entrypoint/RestAuthenticationEntryPoint.java
@@ -8,14 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
 
-@Component("restAuthenticationEntryPoint")
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
+		authException.printStackTrace();
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 	}
 
